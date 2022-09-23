c42c33128029ce73734ecf96de5ecc6cea9c9c48
Elvin Aupiais--Berthy <elvin.aupiais-berthy@efrei.net>
2022-09-23 09:59:23 +0200

    cleaned the raw data

--- cleaner.py
+++ cleaner.py
@@ -13,15 +13,23 @@ def open_with_json(path):
     return data
 
 def item_data(item):
-    name = item['name']
-    image = item['image']
-    price = item['price']
-    currency = item['currency']
-    extendedName = item['extendedName']
-    print(f"{name=}\n{image=}\n{price=}\n{currency=}\n{extendedName=}")
+    new = {}
+    new['name'] = item['name']
+    new['image'] = item['image']
+    new['price'] = item['price']
+    new['currency'] = item['currency']
+    new['extendedName'] = item['extendedName']
+    new['urlId'] = item['urlId']
+    new['url'] = f"https://ldlc.com/fiche/{new['urlId']}.html"
+    return new
 
 if __name__ == "__main__":
-    data = open_with_json("processeurs.json")
+    fileName = "aio_coolers"
+    data = open_with_json(fileName+'.json')
     print(data.keys())
-    first_item = data["data"][0]
-    item_data(first_item)
+    item_list = []
+    for item in data['data']:
+        new = item_data(item)
+        item_list.append(new)
+    with open(fileName+"_cleaned.json",'w') as file:
+        json.dump(item_list, file)
