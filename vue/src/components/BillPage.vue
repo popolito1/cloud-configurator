3d29c0d9880a7cbbbd28ed404cfe5e300f92b2bd
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2022-10-09 17:00:21 +0200

    Simulate a shopping cart

--- vue/src/components/BillPage.vue
+++ vue/src/components/BillPage.vue
@@ -1,14 +1,37 @@
 <template>
     <div class="container">
-        
+        <button @click="simulateCart">Simulate cart</button>
     </div>
 </template>
   
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import { useStore } from '../store';
+    import { Product } from '../store';
 
     export default defineComponent({
         name: 'BillPage',
+        data(){
+            return {
+                store: useStore(),
+                cart: new Array<Product>(),
+            }
+        },
+        methods:{
+            async getCart(){
+                let array = [{urlId: "PB00340585", category:"aio_coolers"},{urlId: "PB00505045", category: "graphic_cards"},{urlId: "PB00346166", category: "motherboards"},{urlId: "PB00493654", category: "processeurs"},{urlId: "PB00400847", category: "psu"},{urlId: "PB00271616", category: "ram"}]
+                let cart = new Array<Product>();
+                for (const element of array) {
+                    let product = await this.store.dispatch("getProduct",element)
+                    cart.push(product)
+                }
+                this.cart = cart
+            },
+            async simulateCart(){
+                await this.getCart();
+                console.log(this.cart)
+            }
+        }
     });
 </script>
   
