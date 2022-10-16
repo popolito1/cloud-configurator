d8e5c12f16d284027df3adcca5c678da161b58f7
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2022-10-16 17:36:29 +0200

    Adding method to switch page to choose the product

--- vue/src/components/ConfiguratorElement.vue
+++ vue/src/components/ConfiguratorElement.vue
@@ -3,7 +3,7 @@
         <img :src="require(`@/assets/configuratorElements/${image}`)">
         <h2>{{category}}</h2>
         <div class="buttons">
-            <ConfiguratorElementButton v-if="product==null" color="add">
+            <ConfiguratorElementButton @click= addProduct() v-if="product==null" color="add">
                 <font-awesome-icon :icon="['fas','plus']"/>
             </ConfiguratorElementButton>
             <ConfiguratorElementButton v-if="product!=null" color="edit">
@@ -22,18 +22,49 @@
 
     export default defineComponent({
         name: 'ConfiguratorElement',
-        data(){
-            return {
-                product: null
-            }
-        },
         components:{
             ConfiguratorElementButton
         },
         props: {
             category: String,
-            image: String
-        }
+            image: String,
+            product: {
+                type: Object,
+                default: null
+            }
+        },
+        methods:{
+            addProduct(){
+                let categoryName;
+                switch (this.category) {
+                    case "Ventirad":
+                        categoryName="aio_coolers";
+                        break;
+                    case "Carte graphique":
+                        categoryName="graphic_cards";
+                        break;
+                    case "Carte mère":
+                        categoryName="motherboards";
+                        break;
+                    case "Processeur":
+                        categoryName="processeurs";
+                        break;
+                    case "Alimentation":
+                        categoryName="psu";
+                        break;
+                    case "Mémoire vive":
+                        categoryName="ram";
+                        break;
+                    case "Boitier":
+                        categoryName="cases";
+                        break;
+                    default:
+                        categoryName=this.category;
+                        break;
+                }
+                this.$router.push("/configurator/"+categoryName);
+            }
+        },
     });
 
 </script>
