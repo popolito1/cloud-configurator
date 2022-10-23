d3eb514a3a61e7a9dc34f85f585a2d2892cbd5f7
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2022-10-23 22:41:25 +0200

    Adding file download icon

--- vue/src/lib/fontAwesome.ts
+++ vue/src/lib/fontAwesome.ts
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {faPen, faTrash, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faPen, faTrash, faPlus, faSearch, faFileDownload } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPen, faTrash, faPlus, faSearch );
+library.add(faPen, faTrash, faPlus, faSearch, faFileDownload);
 export { FontAwesomeIcon }
 
