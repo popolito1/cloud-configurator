775f3aef693d6f89342b2723f8a6517e329bce65
popolito1 <paulineblanc92@gmail.com>
2024-01-31 20:44:29 +0100

    fix: dbservice yml file no need dockerfile

--- data/src/data-source.ts
+++ data/src/data-source.ts
@@ -10,12 +10,13 @@ require("dotenv").config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "dbservice",
+    host: "localhost",
     port: 3306,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DATABASE, 
     synchronize: true,
     logging: false,
-    entities: [Product, Compat, Basket, BasketProduct],
+    entities: ['**/src/entity/**/*.ts'],
+
 })
